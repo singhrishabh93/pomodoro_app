cb3195015aa007b9870207a676ff2501078d4e2e
Rishabh Singh <singhrishabh1670@gmail.com>
2025-03-12 20:00:15 +0530

    sound added

--- lib/services/sound_service.dart
+++ lib/services/sound_service.dart
@@ -10,29 +10,32 @@ class SoundService {
   }
   
   void playStart() async {
-    await _playSound('assets/sounds/start.m4a');
+    await _playSound('sounds/start.m4a');
   }
   
   void playShortBreakStart() async {
-    await _playSound('assets/sounds/short_break_start.m4a');
+    await _playSound('sounds/short_break_start.m4a');
   }
   
   void playShortBreakEnd() async {
-    await _playSound('assets/sounds/short_break_end.m4a');
+    await _playSound('sounds/short_break_end.m4a');
   }
   
   void playLongBreakStart() async {
-    await _playSound('assets/sounds/long_break_start.m4a');
+    await _playSound('sounds/long_break_start.m4a');
   }
   
   void playLongBreakEnd() async {
-    await _playSound('assets/sounds/long_break_end.m4a');
+    await _playSound('sounds/long_break_end.m4a');
   }
   
   Future<void> _playSound(String assetPath) async {
-    await _audioPlayer.stop();
-    await _audioPlayer.play(AssetSource(assetPath));
-  }
+  await _audioPlayer.stop();
+  String fixedPath = assetPath.startsWith('assets/') 
+      ? assetPath.substring(7) 
+      : assetPath;
+  await _audioPlayer.play(AssetSource(fixedPath));
+}
   
   void dispose() {
     _audioPlayer.dispose();
